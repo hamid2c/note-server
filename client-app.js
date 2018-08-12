09f50f2bd4abe9eb15c07aa743b1407b8d2d2ff8
Hamid <hamid2c@gmail.com>
2018-08-12 23:33:42 +0200

    work on client, seems almsot done

--- client-app.js
+++ client-app.js
@@ -9,6 +9,18 @@ const TAGS_BOX_NAME = "tags-box";
 const MARKDOWN_BOX_NAME = "markdown-box";
 const MARKDOWN_BOX_REF = "markdown-ref";
 
+/**
+ * @returns a list of tags
+ * @param the string containing all the tags (seperated by ,)
+ */
+function getTags(tags) {
+  if (typeof tags !== "string") {
+    console.log("The parameter should be a string");
+    return [];
+  }
+  return tags.split(',').map(token => token.trim());
+}
+
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   timeout: 1000,
@@ -18,12 +30,11 @@ const axiosInstance = axios.create({
 class App extends Component {
   constructor(props) {
     super(props);
-    this.handleServerResponse = this.handleServerResponse.bind(this); 
-    this.state = {serverResponse: "init"}
+    this.handleServerResponse = this.handleServerResponse.bind(this);
+    this.state = { serverResponse: "init" }
   }
   handleServerResponse(response) {
-    this.setState({serverResponse: response})
-    console.log("handle change " + response);
+    this.setState({ serverResponse: response });
   }
   render() {
     return (
@@ -43,65 +54,60 @@ class InputForm extends React.Component {
     this.handleKeyDown = this.handleKeyDown.bind(this);
     this.handleMarkdownChange = this.handleMarkdownChange.bind(this);
     this.handleTagsChange = this.handleTagsChange.bind(this);
-    this.state = {tagsValue: "", markdownValue: ""};
+    this.makeRequest = this.makeRequest.bind(this);
+    this.state = { tagsValue: "", markdownValue: "" };
   }
-  handleKeyDown = function(e) {
-    
-    // if (e.keyCode === 27) {
-    //   console.log('You pressed the escape key!');
-    //   this.props.changeFunc(" something ");
-    //   axiosInstance.post('/note', {
-    //     markdown: 'Fred',
-    //     tags: ['Flintstone', "science"]
-    //   })
-    //   .then(function (response) {
-    //     changeFunc(response);
-    //   })
-    //   .catch(function (error) {
-    //     changeFunc(error);
-    //   });
-    // }
+
+  makeRequest = function () {
+    let tags = getTags(this.state.tagsValue);
+    let markdown = this.state.markdownValue.trim();
+    const thiz = this;
+    axiosInstance.post('/note', {
+      markdown: markdown,
+      tags: tags
+    }).then(function (response) {
+      console.log(response);
+      thiz.props.notifyAboutResponse(response.status);
+    }).catch(function (error) {
+      let errMsg = (error.message === undefined)?"UNKNOWN SERVER ERROR":error.message;
+      thiz.props.notifyAboutResponse(errMsg);
+    });
+    //this.props.notifyAboutResponse("hi " + new Date() + this.state.tagsValue);
+  }
+  handleKeyDown = function (e) {
     const funcs = {};
-    funcs[ESC_KEYCODE] = function(inputBox) {
-      console.log("ESC! " + inputBox.name + " " + inputBox.value);
-      this.setState({markdownValue:"", tagsValue:""});
+    funcs[ESC_KEYCODE] = function (inputBox) {
+      this.setState({ markdownValue: "", tagsValue: "" });
       this.refs[MARKDOWN_BOX_REF].focus();
     }
-    funcs[ALT_KEYCODE] = function(inputBox) {
-      console.log("ALT! "+ inputBox.name + " " + inputBox.value);
-      this.props.notifyAboutResponse("hi " + new Date());
+    funcs[ALT_KEYCODE] = function (inputBox) {
+      this.makeRequest();
     }
     const action = funcs[e.keyCode];
     if (action !== undefined) {
-      console.log("target value is " + e.target.value);
-      if (e.target.name === TAGS_BOX_NAME) {
-        
-      } else if (e.target.name === MARKDOWN_BOX_NAME) {
-        
-      }
       action.apply(this, [e.target]);
     }
   }
 
-  handleTagsChange = function(e) {
-    this.setState({tagsValue: e.target.value});
+  handleTagsChange = function (e) {
+    this.setState({ tagsValue: e.target.value });
   }
 
-  handleMarkdownChange = function(e) {
-    this.setState({markdownValue: e.target.value});
+  handleMarkdownChange = function (e) {
+    this.setState({ markdownValue: e.target.value });
   }
 
   render() {
     return (
       <form>
-        
+
         Markdown: <br />
-        <textarea ref={MARKDOWN_BOX_REF} name={MARKDOWN_BOX_NAME}  value={this.state.markdownValue} 
-        onKeyDown={this.handleKeyDown} onChange={this.handleMarkdownChange} 
-        autoFocus rows="33" cols="40" />
+        <textarea ref={MARKDOWN_BOX_REF} name={MARKDOWN_BOX_NAME} value={this.state.markdownValue}
+          onKeyDown={this.handleKeyDown} onChange={this.handleMarkdownChange}
+          autoFocus rows="33" cols="40" />
         <br />
-        Tags: <input type='text' name={TAGS_BOX_NAME} value={this.state.tagsValue} 
-                onKeyDown={this.handleKeyDown} onChange={this.handleTagsChange}  />       
+        Tags: <input type='text' name={TAGS_BOX_NAME} value={this.state.tagsValue}
+          onKeyDown={this.handleKeyDown} onChange={this.handleTagsChange} />
       </form>
     );
   }
